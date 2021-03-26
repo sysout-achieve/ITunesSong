7ad814a81242e586eeb9ae5d27a5f234f458a8b1
gunt <0408gun@gmail.com>
2021-03-26 19:51:30 +0900

    [feat] Bottom navigation 기능 추가
    navigation(Jetpack)을 적용하여 Bottom navigation 기능 추가

--- app/src/main/java/com/gunt/itunessong/ui/MainActivity.kt
+++ app/src/main/java/com/gunt/itunessong/ui/MainActivity.kt
@@ -2,14 +2,48 @@ package com.gunt.itunessong.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.gunt.itunessong.R
+import com.gunt.itunessong.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setContentView(binding.root)
 
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_container) as NavHostFragment
+
+        navController = navHostFragment.navController
+        setupActionBarWithNavController(navController)
+
+        setupBottomNavigationSelectedListener()
+    }
+
+    private fun setupBottomNavigationSelectedListener() {
+        binding.bottomNav.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.song -> {
+                    navController.setGraph(R.navigation.nav_songs)
+                    setupActionBarWithNavController(navController)
+                }
+                R.id.favorite -> {
+                    navController.setGraph(R.navigation.nav_favorites)
+                    setupActionBarWithNavController(navController)
+                }
+            }
+            return@setOnNavigationItemSelectedListener true
+        }
     }
 }
+
---
+++ app/src/main/java/com/gunt/itunessong/ui/songs/SongsFragment.kt
@@ -0,0 +1,18 @@
+package com.gunt.itunessong.ui.songs
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.gunt.itunessong.R
+
+class SongsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_songs, container, false)
+    }
+}
