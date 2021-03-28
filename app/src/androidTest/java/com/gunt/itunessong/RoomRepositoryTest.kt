2cab24a694d46f5b90864a912677e47d2ab630a3
gunt <0408gun@gmail.com>
2021-03-28 22:29:22 +0900

    [test] RoomRepositoryTest 테스트 코드 추가
    Room Database에서 사용되는 insert, read, delete 테스트 구현

---
+++ app/src/androidTest/java/com/gunt/itunessong/RoomRepositoryTest.kt
@@ -0,0 +1,120 @@
+package com.gunt.itunessong
+
+import android.content.Context
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.google.common.truth.Truth.assertThat
+import com.gunt.itunessong.data.repository.db.AppDatabase
+import com.gunt.itunessong.data.repository.db.entity.TrackEntity
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Before
+import org.junit.FixMethodOrder
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.MethodSorters
+
+
+@RunWith(AndroidJUnit4::class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+class RoomRepositoryDaoTest {
+
+    private val context = ApplicationProvider.getApplicationContext<Context>()
+    private lateinit var appDatabase: AppDatabase
+
+    private val trackEntity =  TrackEntity(
+        trackId = 1,
+        artistName = "greenday",
+        collectionName = "greendayCollection",
+        trackName = "greendayTrackName",
+        artworkUrl60 = "https://is2-ssl.mzstatic.com/image/thumb/Music114/v4/77/4b/20/774b2007-2198-8fa3-1491-8c0e3b0ce4f2/source/60x60bb.jpg"
+    )
+
+    @Before
+    fun setup() {
+        appDatabase = Room.inMemoryDatabaseBuilder(
+            context,
+            AppDatabase::class.java
+        ).build()
+    }
+
+    @Test
+    fun trackDao1InsertTest() = runBlocking {
+        //when
+        appDatabase.trackDAO().insert(trackEntity)
+
+        //then
+        assertThat(appDatabase.trackDAO().getAll()[0]).isEqualTo(trackEntity)
+    }
+
+    @Test
+    fun trackDao2DeleteTest() = runBlocking {
+        //given
+        // trackDao1InsertTest()
+        //when
+        appDatabase.trackDAO().delete(trackEntity)
+
+        //then
+        assertThat(appDatabase.trackDAO().getAll()).isEmpty()
+    }
+
+    @Test
+    fun trackDao3InsertListTest() = runBlocking {
+        //given
+        val expected = 31
+        val list = mutableListOf<TrackEntity>()
+        for (i in 1..expected){
+            list.add(
+                TrackEntity(
+                    trackId = i.toLong(),
+                    artistName = "greenday",
+                    collectionName = "greendayCollection",
+                    trackName = "greendayTrackName",
+                    artworkUrl60 = "https://is2-ssl.mzstatic.com/image/thumb/Music114/v4/77/4b/20/774b2007-2198-8fa3-1491-8c0e3b0ce4f2/source/60x60bb.jpg"
+                )
+            )
+        }
+        //when
+        appDatabase.trackDAO().insertTracksAll(list)
+
+        //then
+        val resultList = appDatabase.trackDAO().getAll()
+        assertThat(resultList.size).isNotEqualTo(0)
+        assertThat(resultList.size).isEqualTo(expected)
+    }
+
+    @Test
+    fun trackDao4FetchTest() = runBlocking {
+        //given
+        val listInsert = mutableListOf<TrackEntity>()
+        for (i in 1..31){
+            listInsert.add(
+                TrackEntity(
+                    trackId = i.toLong(),
+                    artistName = "greenday",
+                    collectionName = "greendayCollection",
+                    trackName = "greendayTrackName",
+                    artworkUrl60 = "https://is2-ssl.mzstatic.com/image/thumb/Music114/v4/77/4b/20/774b2007-2198-8fa3-1491-8c0e3b0ce4f2/source/60x60bb.jpg"
+                )
+            )
+        }
+        appDatabase.trackDAO().insertTracksAll(listInsert)
+
+        //when
+        val list = appDatabase.trackDAO().fetchTracks(10,0)
+
+        //then
+        assertThat(list.size).isEqualTo(10)
+
+        // 저장된 31개의 데이터 중에 limit 10 offset 30일 경우 -> 기대값 = 1
+        val expected = 1
+        val list2 = appDatabase.trackDAO().fetchTracks(10,30)
+        assertThat(list2.size).isEqualTo(expected)
+    }
+
+    @After
+    fun cleanup() = runBlocking {
+        appDatabase.trackDAO().deleteAll()
+    }
+}
